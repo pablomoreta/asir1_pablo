0420d1748660d0dd792b319d3757240bfa2d0563
Pablo <pablo.moreta.lopez@gmail.com>
2019-10-03 18:30:34 +0200

    .

--- triangulos_a.php
+++ triangulos_a.php
@@ -1,25 +1,14 @@
-<?php 
-$a1=$_GET['a1']; 
-$a2=$_GET['a2']; 
-$a3=$_GET['a3']; 
-$suma=$a1+$a2+$a3; 
-
-if ($suma!=180) { 
-       
-    echo "<p>No se corresponde a un triángulo</p>"; 
-} else {  
-    if ($a1==$a2 && $a2==$a3) { 
-        
-        echo "<p>El triángulo es equilátero</p>"; 
-    } else { 
-                   if ($a1==$a2 || $a2==$a3 || $a1==$a3) { 
-                      
-                echo "<p>El triángulo es isósceles</p>"; 
-        } else {  
-					if ($a1!=a2!=a3){
-                echo "<p>El triángulo es escaleno</p>"; 
-	     } else {
-            }  
-        } 
-    } 
+<?php
+function tipo($a, $b, $c) {
+	$r='escaleno';
+		if ($a==$b || $a==$c) {
+			$r='isosceles';
+		if($b==$c)
+			$r='equilatero';
+	}
+	return $r;
+}
+echo tipo(1,1,1)."<br>";
+echo tipo(1,1,6)."<br>";
+echo tipo(2,3,4)."<br>";
 ?>
