1e9e4f9f336dcab63d7db1e03c02a21bbe7900a7
pablomoreta <55439163+pablomoreta@users.noreply.github.com>
2019-10-23 20:43:23 +0200

    html

---
+++ html.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<title>Santander</title>
+</head>
+<body bgcolor="aquamarine">
+<center>
+<h1 align="center">Santander</h1>
+<h2 align="center">(Cantabria)</h2>
+<h3 style="color:red"><u><b>Lugares de interés</u></b></h3>
+<font style="font-size:15px" face="Comic Sans MS">
+Palacio De la Magdalena<br>El Sardinero,Playa,Casino y Surf<br>Península De la Magdalena<br>Centro Botín<br>Catedral De Santander<br>Jardines De Piquío<br>Faro De Cabo Mayor<br>Parque y Playa De Mataleñas<br>Playa De la Maruca<br>Muralla Medieval<br>Ruta Del Incendio<br>
+</font>
+<h4 style="color:red"><u><b>Localización</u></b></h4>
+<font color="purple" style="font-size:15px" face="italic">
+
+<p>La ciudad de Santander se sitúa al norte de España bañada por el mar Cantábrico y pertenece a la comunidad autónoma de Cantabria.</p>
+
+</font>
+</br>
+<img src="localizacion.png" width="200">
+<img src="santander.jpg" width="230">
+<img src="centro.jpg" width="420">
+</br>
+
+<table border="2" bordercolor="orangered"">
+
+
+
+  <tr align="center">
+
+    <td>Autonomía</td>
+
+    <td>Provincia</td>
+
+  </tr>
+
+<tr align="center">
+	<td>Cantabria</td>
+	<td>Santander</td>
+</tr>
+<tr align="center">
+	<td>Galicia</td>
+	<td>La Coruña</td>
+</tr>
+
+<tr align="center">
+	<td>Galicia</td>
+	<td>Lugo</td>
+</tr>
+
+<tr align="center">
+	<td>Asturias</td>
+	<td>Gijón</td>
+</tr>
+
+<tr align="center">
+	<td>País Vasco</td>
+	<td>Bilbao</td>
+</tr>
+<tr align="center">
+	<td>País Vasco</td>
+	<td>San Sebastián</td>
+</tr>
+</table>
+</br>
+<h4 style="color:red"><u> Rellene el formulario con sus datos personales:</u></h4>
+<form action="datos.php" method="post">
+
+	Nombre: <input type="text" name="nombre"> </br>
+	Email: <input type="email" name="email"> </br>
+	<input type="submit" value="Enviar">
+</form>
+		
+</center>
+</body>
+</html>
