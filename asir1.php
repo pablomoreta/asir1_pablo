bd1ee906749f5a7e49ef5a5580cca2c35730a06c
pablomoreta <55439163+pablomoreta@users.noreply.github.com>
2019-11-03 20:56:20 +0100

    asir1

--- asir1.php
+++ asir1.php
@@ -1,21 +1,14 @@
-<style type="text/css">
-  body {
-    color: purple;
-    background-color: <?php $color = substr(md5(time()), 0, 6); echo "#"; echo $color;?> }
-  </style>
 <?php
-$color = substr(md5(time()), 0, 6);
-echo '<a href="http://192.168.0.96/asir1_aaron/asir1.php">Pagina de aaron</a>';
-echo "<br>";
-
-echo "<br>";
 echo '<a href="http://192.168.0.77/asir1_adrian/asir1.php">Página de adrian</a>';
+echo "<br>";
+echo "Pablo Moreta";
+echo "<br>";
+echo '<a href="http://192.168.0.96/asir1_aaron/asir1.php">Pagina de aaron</a>';
   $archivo="visitas.txt";
-  $user_agent = $_SERVER['HTTP_USER_AGENT'];
   $proceso=fopen($archivo,"a") or die("basura");
   $ip=$_SERVER['REMOTE_ADDR'];
   $hoy = date('Y-m-d H:i:s');
-  $datos="La ip es:".$ip." y fecha: ".$hoy." con Navegador".$user_agent."\r\n";
+  $datos="La ip es:".$ip." y fecha: ".$hoy."\r\n";
   fwrite($proceso,$datos);
   fclose($proceso);
-  ?>
+?>
