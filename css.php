b351571410d104ca2d9fa86466875e1b6ada3f4d
Pablo <pablo.moreta.lopez@gmail.com>
2019-11-12 12:23:15 +0100

    css

---
+++ css.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+<title>Santander</title>
+ <style type="text/css">
+  body {
+    color: red;
+  background-color: #d8da3d }
+	h1 {
+	text-align:center;
+	text-decoration: underline slateblue;
+	}
+	p {
+  color: navy;
+  width: 820px;
+  border: 1px solid black;
+  font-weight: bold;
+  text-align:center;
+}
+  </style>
+</head>
+<body>
+
+<h1>Santander</h1>
+<h2 align="center">(Cantabria)</h2>
+<h4><u><b>Lugares de interés</u></b></h4>
+<font color="red" size="2px" face="Comic Sans MS">
+Palacio De la Magdalena<br>El Sardinero,Playa,Casino y Surf<br>Península De la Magdalena<br>Centro Botín<br>Catedral De Santander<br>Jardines De Piquío<br>Faro De Cabo Mayor<br>Parque y Playa De Mataleñas<br>Playa De la Maruca<br>Muralla Medieval<br>Ruta Del Incendio<br>
+
+</font>
+
+<font face="cambria" marginwidth="20" font size="2"><h4><b>Localización</b></h4>
+
+<p>La ciudad de Santander se sitúa al norte de España bañada por el mar Cantábrico y pertenece a la comunidad autónoma de Cantabria.</p>
+
+</font>
+</br>
+<img src="localizacion.png" width="200">
+<img src="santander.jpg" width="230">
+<img src="centro.jpg" width="420">
+<table border="2" bordercolor="orangered"">
+  <tr align="center">
+    <td bgcolor="orange">Autonomía</td>
+    <td bgcolor="orange">Provincia</td>
+  </tr>
+<tr align="center">
+	<td>Cantabria</td>
+	<td>Santander</td>
+</tr>
+<tr align="center">
+	<td>Galicia</td>
+	<td>La Coruña</td>
+</tr>
+<tr align="center">
+	<td>Galicia</td>
+	<td>Lugo</td>
+</tr>
+<tr align="center">
+	<td>Asturias</td>
+	<td>Gijón</td>
+</tr>
+<tr align="center">
+	<td>País Vasco</td>
+	<td>Bilbao</td>
+</tr>
+<tr align="center">
+	<td>País Vasco</td>
+	<td>San Sebastián</td>
+</tr>
+</table>
+<p>Formulario De Suscripción:</p>
+<form action="santander.php" method="post">
+	Nombre: <input type="text" name="nombre"> </br>
+	DNI: <input type="text" name="DNI"> </br>
+	Email: <input type="email" name="email"> </br>
+	<input type="submit" value="Enviar">
+</form>
+
+</body>
+</html>
